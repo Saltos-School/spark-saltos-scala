60619aaba65ab7d039dfc404d63153353e11517d
Carlos Saltos <csaltos@gmail.com>
2023-06-09 12:42:19 +0200

    Adding Movies

--- src/main/scala/com/saltos/school/spark/Movies.scala
+++ src/main/scala/com/saltos/school/spark/Movies.scala
@@ -28,17 +28,32 @@ object Movies {
     ratingsDF.show()
     ratingsDF.printSchema()
 
+    val linksDF = loadLinksDF(spark, dataPath).cache()
+    linksDF.show()
+    linksDF.printSchema()
+
     val moviesWithRatings = moviesDF.join(ratingsDF, "movieId")
     moviesWithRatings.show()
     moviesWithRatings.printSchema()
+
+    // val moviesWithRatingsAndLinks = moviesWithRatings.join(linksDF, "movieId")
 
     val userId = "5"
     val userMoviesRatings = moviesWithRatings.filter("userId = " + userId).cache()
     userMoviesRatings.show()
     println("Ratings por usuario: " + userMoviesRatings.count())
 
+    val userMoviesRatingsWithLinks = userMoviesRatings.join(linksDF, "movieId")
+
+    /*userMoviesRatings flatMap { row =>
+      val movieId = row.get(0)
+      linksDF.filter("movieId = " + movieId) map { row =>
+        row.get(2)
+      }
+    }*/
+
     import org.apache.spark.sql.functions.desc
-    val userMoviesTopRatings = userMoviesRatings.sort(desc("rating"))
+    val userMoviesTopRatings = userMoviesRatingsWithLinks.sort(desc("rating"))
     userMoviesTopRatings.show(1000)
 
     val userMoviesTop10Ratings = userMoviesTopRatings.take(10)
@@ -46,7 +61,8 @@ object Movies {
     userMoviesTop10Ratings.foreach { movieRow =>
       val title = movieRow.get(1)
       val rating = movieRow.getAs[Double]("rating")
-      println("Titulo: " + title + ", Estrellas: " + rating)
+      val imdbId = movieRow.getAs[String]("imdbId")
+      println("Titulo: " + title + ", Estrellas: " + rating + ", Link: http://www.imdb.com/title/tt" + imdbId + "/")
     }
 
     spark.stop()
@@ -68,6 +84,15 @@ object Movies {
     val timestampField = DataTypes.createStructField("timestamp", DataTypes.LongType, true)
     val ratingsSchema = DataTypes.createStructType(Array(userIdField, movieIdField, ratingField, timestampField))
     val ratingsDF = spark.read.option("header", true).schema(ratingsSchema).csv(dataPath + "ratings.csv")
+    ratingsDF
+  }
+
+  def loadLinksDF(spark: SparkSession, dataPath: String) = {
+    val movieIdField = DataTypes.createStructField("movieId", DataTypes.LongType, true)
+    val imdbIdField = DataTypes.createStructField("imdbId", DataTypes.StringType, true)
+    val tmdbIdField = DataTypes.createStructField("tmdbId", DataTypes.LongType, true)
+    val ratingsSchema = DataTypes.createStructType(Array(movieIdField, imdbIdField, tmdbIdField))
+    val ratingsDF = spark.read.option("header", true).schema(ratingsSchema).csv(dataPath + "links.csv")
     ratingsDF
   }
 
